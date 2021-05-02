7630c02a023d54971924f71c4df29a4eebb8b76e
rajesh-ragam <74100130+rajesh-ragam@users.noreply.github.com>
2021-05-02 12:21:14 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "RUNNING THE SCRIPT NOW...!!"
 echo "......................."
-OS=$(cat /etc/lsb-release)
+$OS=$(cat /etc/lsb-release)
 echo "this os delails are: $OS"
 echo "......................."
