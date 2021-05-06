7f02a551b977e9ffc148cd3d18dde238b525ecf9
rajesh-ragam <74100130+rajesh-ragam@users.noreply.github.com>
2021-05-06 18:18:04 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 VERSION=$(date +%H-%M-%S)
-docker build -t rajeshragam/jenkinsjob:${VERSION} .
+docker build -t sreeharshav/testingbuild:${VERSION} .
 docker push rajeshragam/jenkinsjob:${VERSION}
 docker -H tcp://172-31-38-105:2375 stop nginx
 docker -H tcp://172-31-38-105:2375 run --rm -dit -p 8000:80 --name nginx --hostname nginx rajeshragam/jenkinsjob:${VERSION}
